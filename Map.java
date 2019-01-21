f71d11cb2028e58666d055764a4f402ce61322fa
Lastfive1 <45343005+Lastfive1@users.noreply.github.com>
2019-01-21 11:43:23 -0500

    Update Map.java

--- Map.java
+++ Map.java
@@ -13,8 +13,9 @@ import javafx.scene.shape.Line;
 import javafx.scene.image.*;
 
 public class Map extends Application {
-	double simonX;
-	double simonY;
+	// double simonX;
+	private Simon simon;
+	// double simonY;
 	double BaddyX;
 	double BaddyY;
 	double BaddyX2;
@@ -23,7 +24,7 @@ public class Map extends Application {
 	double BaddyY3;
 	double BaddyX4;
 	double BaddyY4;
-	private Circle Simon;
+	//private Circle Simon;
 	private Scene map;
 	private ImageView key;
 	private ImageView masterdoor;
@@ -36,14 +37,12 @@ public class Map extends Application {
 	private Boolean Win_zone = false;
 	private Boolean Baddymessage = false;
 	private Group root;
-	private Boolean move = true;
 	private Rectangle end;
 	private Rectangle Baddy;
 	private Rectangle Baddy2;
 	private Rectangle Baddy3;
 	private Rectangle Baddy4;
 	private Boolean trapdeathmessage = false;
-	private Boolean Dead = false;
 	private Line room1;
 	private Line room1pt2;
 	private Line room2;
@@ -65,8 +64,8 @@ public class Map extends Application {
 		myArray[2] = 3;
 		myArray[3] = 4;
 		myArray[4] = 5;
-		key = new ImageView(new Image("key.png"));
-		masterdoor = new ImageView(new Image("door1.jpg"));
+		key = new ImageView(new Image("key1.jpg"));
+		masterdoor = new ImageView(new Image("door.jpg"));
 		trap1 = new ImageView(new Image("sportnite.png"));
 		trap2 = new ImageView(new Image("sportnite.png"));
 		trap3 = new ImageView(new Image("sportnite.png"));
@@ -79,9 +78,7 @@ public class Map extends Application {
 		key.setX(20);
 		key.setY(160);
 		masterdoor.setX(130);
-		masterdoor.setY(450);
-		simonX = 30;
-		simonY = 30;
+		masterdoor.setY(442);
 		BaddyX = 150;
 		BaddyY = 10;
 		BaddyX2 = 10;
@@ -99,8 +96,8 @@ public class Map extends Application {
 		end = new Rectangle(420, 440, 70, 50);
 		end.setFill(Color.GOLD);
 		stage.setFill(Color.WHITE);
-		Simon = new Circle(simonX, simonY, 15);
-		Simon.setFill(Color.BLACK);
+		simon = new Simon(30, 30, 15);
+		simon.setColour(Color.BLACK);
 		room1 = new Line(10, 150, 280, 150);
 		room1pt2 = new Line(360, 150, 490, 150);
 		Line keyroom = new Line(10, 200, 30, 200);
@@ -109,22 +106,22 @@ public class Map extends Application {
 		room2 = new Line(360, 150, 360, 380);
 		room2pt2 = new Line(360, 380, 150, 380);
 		room2pt3 = new Line(150, 380, 150, 445);
-		root.getChildren().addAll(stage, end, Simon, trap1, trap2, trap3, key, room1, room1pt2, keyroom, keyroompt2,
+		root.getChildren().addAll(stage, end,simon.getCircle() , trap1, trap2, trap3, key, room1, room1pt2, keyroom, keyroompt2,
 				keyroompt3, room2, room2pt2, room2pt3, masterdoor, Baddy, Baddy2, Baddy3, Baddy4);
 
-		map.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				if (event.getCode() == KeyCode.W && move == true)
-					simonY -= 3;
-				else if (event.getCode() == KeyCode.S && move == true)
-					simonY += 3;
-				else if (event.getCode() == KeyCode.D && move == true)
-					simonX += +3;
-				else if (event.getCode() == KeyCode.A && move == true)
-					simonX -= 3;
-			}
-		});
+		 map.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		 @Override
+		 public void handle(KeyEvent event) {
+		 if (event.getCode() == KeyCode.W)
+			 simon.setSimonY(simon.getSimonY()-3);
+		 else if (event.getCode() == KeyCode.S)
+			 simon.setSimonY(simon.getSimonY()+3);
+		else if (event.getCode() == KeyCode.D)
+			simon.setSimonX(simon.getSimonX()+3);
+		 else if (event.getCode() == KeyCode.A)
+			 simon.setSimonX(simon.getSimonX()-3);
+		 }
+		 });
 		AnimationTimer timer = new AnimationTimer() {
 			public void handle(long arg0) {
 				upDate();
@@ -138,77 +135,80 @@ public class Map extends Application {
 	}
 
 	public void upDate() {
-		Simon.setCenterX(simonX);
-		Simon.setCenterY(simonY);
-		if (Simon.getBoundsInParent().intersects(room1.getBoundsInParent())) {
-			simonY -= 5;
-			Simon.setCenterY(simonY);
+		Circle circle = simon.getCircle();
+		circle.setCenterX(simon.getSimonX());
+		circle.setCenterY(simon.getSimonY());
+		if (circle.getBoundsInParent().intersects(room1.getBoundsInParent())) {
+			simon.setSimonY(simon.getSimonY()-5);
+			circle.setCenterY(simon.getSimonY());
 		}
-		if (Simon.getBoundsInParent().intersects(room1pt2.getBoundsInParent())) {
-			simonY -= 5;
-			Simon.setCenterY(simonY);
+		if (circle.getBoundsInParent().intersects(room1pt2.getBoundsInParent())) {
+			simon.setSimonY(simon.getSimonY()-5);
+			circle.setCenterY(simon.getSimonY());
 		}
-		if (Simon.getBoundsInParent().intersects(room2.getBoundsInParent())) {
-			simonX -= 5;
-			Simon.setCenterX(simonX);
+		if (circle.getBoundsInParent().intersects(room2.getBoundsInParent())) {
+			simon.setSimonX(simon.getSimonX()-5);
+			circle.setCenterX(simon.getSimonX());
 		}
-		if (Simon.getBoundsInParent().intersects(room2pt2.getBoundsInParent())) {
-			simonY -= 5;
-			Simon.setCenterY(simonY);
+		if (circle.getBoundsInParent().intersects(room2pt2.getBoundsInParent())) {
+			simon.setSimonY(simon.getSimonY()-5);
+			circle.setCenterY(simon.getSimonY());
 		}
-		if (Simon.getBoundsInParent().intersects(room2pt3.getBoundsInParent())) {
-			simonX -= 5;
-			Simon.setCenterX(simonX);
+		if (circle.getBoundsInParent().intersects(room2pt3.getBoundsInParent())) {
+			simon.setSimonX(simon.getSimonX()-5);
+			circle.setCenterX(simon.getSimonX());
 		}
-		if (Simon.getBoundsInParent().intersects(keyroompt3.getBoundsInParent())) {
-			simonX += 5;
-			Simon.setCenterX(simonX);
+		if (circle.getBoundsInParent().intersects(keyroompt3.getBoundsInParent())) {
+			simon.setSimonX(simon.getSimonX()+5);
+			circle.setCenterX(simon.getSimonX());
 		}
-		if (simonX - 15 <= 10) {
-			simonX += 5;
-			Simon.setCenterX(simonX);
+		if (simon.getSimonX() - 15 <= 10) {
+			simon.setSimonX(simon.getSimonX()+5);
+			circle.setCenterX(simon.getSimonX());
 		}
-		if (simonX + 15 >= 490) {
-			simonX -= 5;
-			Simon.setCenterX(simonX);
+		if (simon.getSimonX() + 15 >= 490) {
+			simon.setSimonX(simon.getSimonX()-5);
+			circle.setCenterX(simon.getSimonX());
 		}
-		if (simonY - 15 <= 10) {
-			simonY += 5;
-			Simon.setCenterY(simonY);
+		if (simon.getSimonY() - 15 <= 10) {
+			simon.setSimonY(simon.getSimonX()+5);
+			circle.setCenterY(simon.getSimonY());
 		}
-		if (simonY + 15 >= 490) {
-			simonY -= 5;
-			Simon.setCenterY(simonY);
+		if (simon.getSimonY() + 15 >= 490) {
+			simon.setSimonY(simon.getSimonX()-5);
+			circle.setCenterY(simon.getSimonY());
 		}
 		Baddy.setY(BaddyY);
 		Baddy2.setX(BaddyX2);
 		Baddy3.setX(BaddyX3);
 		Baddy4.setY(BaddyY4);
-		Win(simonX, simonY);
-		Keyroom(simonY);
-		Trap(simonX, simonY);
-		Baddy(simonX, simonY);
-		Baddy2(simonX, simonY);
-		Baddy3(simonX, simonY);
-		Baddy4(simonX, simonY);
+		Win(simon.getSimonX(), simon.getSimonY());
+		Keyroom(simon.getSimonY());
+		Trap(simon.getSimonX(), simon.getSimonY());
+		Baddy(simon.getSimonX(), simon.getSimonY());
+		Baddy2(simon.getSimonX(), simon.getSimonY());
+		Baddy3(simon.getSimonX(), simon.getSimonY());
+		Baddy4(simon.getSimonX(), simon.getSimonY());
 	}
 
 	public void Win(double simonX, double simonY) {
+		Circle circle = simon.getCircle();
 		if (!Win_zone)
-			if (Simon.getBoundsInParent().intersects(end.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(end.getBoundsInParent())) {
 				Win_zone = true;
 				System.out.println("Victory Royale");
 			}
 	}
 
 	public void Keyroom(double simonY) {
+		Circle circle = simon.getCircle();
 		if (!Key_touching)
-			if (Simon.getBoundsInParent().intersects(key.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(key.getBoundsInParent())) {
 				Key_touching = true;
 				System.out.println("got the key");
 				root.getChildren().get(6).setVisible(false);
 			}
-		if (Simon.getBoundsInParent().intersects(masterdoor.getBoundsInParent())) {
+		if (circle.getBoundsInParent().intersects(masterdoor.getBoundsInParent())) {
 			if (Key_touching == true) {
 				door_passing = true;
 				root.getChildren().get(15).setVisible(false);
@@ -216,43 +216,42 @@ public class Map extends Application {
 				if (door_passing == false) {
 					door_passingmessage = true;
 					System.out.println("get a key");
-					move = false;
-					simonX -= 10;
-					move = true;
+					simon.setSimonX(simon.getSimonX()-10);
 					door_passingmessage = false;
 				}
 		}
 	}
 
 	public void Trap(double simonX, double simonY) {
+		Circle circle = simon.getCircle();
 		if (!trapdeathmessage)
-			if (Simon.getBoundsInParent().intersects(trap1.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(trap1.getBoundsInParent())) {
 				trapdeathmessage = true;
 				System.out.println("You're dead");
-				Dead= true;
+
 				root.getChildren().get(2).setVisible(false);
 
 			}
 		if (!trapdeathmessage)
-			if (Simon.getBoundsInParent().intersects(trap2.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(trap2.getBoundsInParent())) {
 				trapdeathmessage = true;
 				System.out.println("You're dead");
-				Dead = true;
+
 				root.getChildren().get(2).setVisible(false);
 
 			}
 		if (!trapdeathmessage)
-			if (Simon.getBoundsInParent().intersects(trap3.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(trap3.getBoundsInParent())) {
 				trapdeathmessage = true;
 				System.out.println("You're dead");
-				Dead = true;
+
 				root.getChildren().get(2).setVisible(false);
 
 			}
 	}
 
 	public void Baddy(double simonX, double Y) {
-
+		Circle circle = simon.getCircle();
 		if ((BaddyY + 25) <= 150) {
 			BaddyY += 1;
 		} else {
@@ -260,32 +259,33 @@ public class Map extends Application {
 			Baddy.setY(BaddyY);
 		}
 		if (!Baddymessage)
-			if (Simon.getBoundsInParent().intersects(Baddy.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(Baddy.getBoundsInParent())) {
 				Baddymessage = true;
 				System.out.println("Your're dead");
-				Dead = true;
+
 				root.getChildren().get(2).setVisible(false);
 			}
 	}
 
 	public void Baddy2(double simonX, double simonY) {
+		Circle circle = simon.getCircle();
 		if ((BaddyX2 + 50) <= 490) {
 			BaddyX2 += myArray[n];
 		} else {
 			BaddyX2 -= 440;
 			Baddy2.setX(BaddyX2);
 			if (!Baddymessage)
-				if (Simon.getBoundsInParent().intersects(Baddy2.getBoundsInParent())) {
+				if (circle.getBoundsInParent().intersects(Baddy2.getBoundsInParent())) {
 					Baddymessage = true;
 					System.out.println("Your're dead");
-					Dead = true;
+
 					root.getChildren().get(2).setVisible(false);
 				}
 		}
 	}
 
 	public void Baddy3(double simonX, double Y) {
-
+		Circle circle = simon.getCircle();
 		if ((BaddyX3) >= 10) {
 			BaddyX3 -= myArray[n];
 		} else {
@@ -293,16 +293,16 @@ public class Map extends Application {
 			Baddy3.setX(BaddyX3);
 		}
 		if (!Baddymessage)
-			if (Simon.getBoundsInParent().intersects(Baddy3.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(Baddy3.getBoundsInParent())) {
 				Baddymessage = true;
 				System.out.println("Your're dead");
-				Dead = true;
+
 				root.getChildren().get(2).setVisible(false);
 			}
 	}
 
 	public void Baddy4(double simonX, double Y) {
-
+		Circle circle = simon.getCircle();
 		if ((BaddyY4 + 50) <= 490) {
 			BaddyY4 += myArray[n];
 		} else {
@@ -310,10 +310,10 @@ public class Map extends Application {
 			Baddy4.setY(BaddyY4);
 		}
 		if (!Baddymessage)
-			if (Simon.getBoundsInParent().intersects(Baddy4.getBoundsInParent())) {
+			if (circle.getBoundsInParent().intersects(Baddy4.getBoundsInParent())) {
 				Baddymessage = true;
 				System.out.println("Your're dead");
-				Dead = true;
+
 				root.getChildren().get(2).setVisible(false);
 			}
 	}
